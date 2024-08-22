c4d05b307411758fa515e82eb891720434194ab0
BhushanUpwanshi <166616679+BhushanUpwanshi@users.noreply.github.com>
2024-08-23 01:46:03 +0530

    Update duplicate_word_sring.java

--- duplicate_word_sring.java
+++ duplicate_word_sring.java
@@ -1,24 +1,24 @@
 // Write a function to find out duplicate words in a given string?
 // String string = "Big black bug bit a big black dog on his big black nose";
 
-import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-	    String str="Big black bug bit a big black dog on his big black nose";
-		Map <String,Integer> strMap=new HashMap<>();
-		String[] sarr= str.toLowerCase().split(" ");
+		String str="Big black bug bit a big black dog on his big black nose";
+		String[] sarr=str.toLowerCase().split(" ");
 		
-		for(String s : sarr){
-		    System.out.println(s);
-		    if(strMap.containsKey(s))
-		        strMap.put(s,strMap.get(s)+1);
+		Map<String,Integer> smap=new HashMap<>();
+		
+		for(String s: sarr){
+		    if(smap.containsKey(s))
+		        smap.put(s,smap.get(s)+1);
 		    else
-		        strMap.put(s,1);
+		        smap.put(s,1);
 		}
-		for(Map.Entry<String,Integer> entry : strMap.entrySet()){
-		    if(entry.getValue() > 1)
-		        System.out.println(entry.getKey()+" - "+entry.getValue());
+		
+		for(Map.Entry<String,Integer> entry : smap.entrySet()){
+		    if(entry.getValue()>1)
+		        System.out.println(entry.getKey()+" : "+entry.getValue());
 		}
 	}
 }
